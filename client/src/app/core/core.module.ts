71d016e88ee8ce113ae8beeab8939457453d7471
Ramiro Castellanos Barrón <ramiro@castellanosbarron.com>
2023-03-14 16:48:05 -0600

    Imports and Exports Settings

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule,
   ]
 })
 export class CoreModule { }
