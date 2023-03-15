6a33e0a095da66abd79f7458dfeed2c0073daa09
Ramiro Castellanos Barrón <ramiro@castellanosbarron.com>
2023-03-14 18:08:47 -0600

    Datepicker Styles

--- client/src/app/shared/components/date-picker/date-picker.component.ts
+++ client/src/app/shared/components/date-picker/date-picker.component.ts
@@ -15,7 +15,7 @@ export class DatePickerComponent implements ControlValueAccessor {
   constructor(@Self() public ngControl: NgControl) {
     this.ngControl.valueAccessor = this;
     this.bsConfig = {
-      containerClass: 'theme-red',
+      containerClass: 'theme-default',
       dateInputFormat: 'DD MMMM YYYY'
     }
    }
