274c9e6b56836ec516942af85a072dffb97fc6bf
Ramiro Castellanos Barrón <ramiro@castellanosbarron.com>
2023-03-15 21:45:40 -0600

    Identity services Added to IdentityServiceExtensions.cs

--- API/Extensions/IdentityServicesExtensions.cs
+++ API/Extensions/IdentityServicesExtensions.cs
@@ -1,9 +1,45 @@
+using System.Text;
+using Core.Entities.Identity;
+using Infrastructure.Data.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 namespace API.Extensions
 {
     public static class IdentityServicesExtensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
+            services.AddDbContext<IdentityContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
+            });
+            
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddSignInManager<SignInManager<AppUser>>();
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding
+                            .UTF8.GetBytes(config["Token:Key"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
             services.AddAuthorization();
             
             return services;
