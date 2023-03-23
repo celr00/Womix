96937a7f9c6ca1cb04dbe1e88d1610516f430b61
Ramiro Castellanos Barrón <66740266+RamiroCastellanosBarron@users.noreply.github.com>
2023-03-23 15:45:15 -0600

    Breadcrumbs

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,12 +4,15 @@ import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent
   ],
   imports: [
     CommonModule,
@@ -19,10 +22,12 @@ import { ToastrModule } from 'ngx-toastr';
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
     }),
+    BreadcrumbModule,
   ],
   exports: [
     NavBarComponent,
     ToastrModule,
+    SectionHeaderComponent,
   ]
 })
 export class CoreModule { }
