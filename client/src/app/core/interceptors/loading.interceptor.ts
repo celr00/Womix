a864e8bb4b539ad8750f3106be234e164281f3fc
Ramiro Castellanos Barrón <ramiro@castellanosbarron.com>
2023-03-29 11:39:18 -0600

    Loading indicators functionality completed

---
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, delay, finalize, identity } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BusyService } from '../services/busy.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  constructor(private busyService: BusyService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (
+      request.url.includes('emailExists') ||
+      request.method === 'DELETE'
+    ) {
+      return next.handle(request);
+    }
+
+    this.busyService.busy();
+    return next.handle(request).pipe(
+      (environment.production ? identity : delay(1000)),
+      finalize(() => this.busyService.idle())
+    )
+  }
+}
